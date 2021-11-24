7d9a1464efc534ce9cec96ce464eace303f288e8
LeeSeungjae00 <seungjae266789@gmail.com>
2021-11-24 19:22:49 +0900

    event log css add

--- src/components/event_log/event_log.jsx
+++ src/components/event_log/event_log.jsx
@@ -7,7 +7,7 @@ export default function EventLog({ date, data }) {
     const total_volume = Object.keys(data)
         .map((event) => data[event].sets * data[event].volume)
         .reduce((pre, curr) => pre + curr);
-    const [check, setCheck] = useState(false);
+    const [check, setCheck] = useState(true);
     const onDetail = () => {
         setCheck(check => !check);
     }
@@ -23,11 +23,11 @@ export default function EventLog({ date, data }) {
                 <button onClick={onDetail} className={styles.detailBtn}>Detail {check ? '☝' : '↓'}</button>
             </div>
             <Collapse in={check}>
-            <ul>
-                {Object.keys(data)
-                    .map((event) => <div>{`${data[event].event} (을)를 ${data[event].volume}kg 씩 ${data[event].sets} 세트 했습니다.`}</div>)}
-            </ul>
-        </Collapse>
+                <ul className = {styles.detailList}>
+                    {Object.keys(data)
+                        .map((event) => <li>{`${data[event].event} (을)를 ${data[event].volume}kg 씩 ${data[event].sets} 세트 했습니다.`}</li>)}
+                </ul>
+            </Collapse>
         </li>
 
     </>
